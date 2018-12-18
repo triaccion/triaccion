a427a237ad641037320317c79167a0e01c7ce6ba
omuro <triacciontenerife@gmail.com>
2018-12-18 18:24:13 +0000

    Commit message2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+    console.log((new Date()) + ' Server is listening on port ' + port);
+
 });
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
-});
+
+app.listen(port);
